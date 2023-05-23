b9ab42c1718add011f331746e3a7c5ab0eda6b8f
chunkybooboo <89316044+chunkybooboo@users.noreply.github.com>
2023-05-22 17:35:16 -0700

    moved files out from the folder

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,7 +4,7 @@ import monaPic from '../../img/mona.jpg';
 
 export default function Home() {
   return (
-    <div style={{ marginInline: 700, textAlign: 'center', display: 'inline-block'}}> 
+    <div style={{ marginInline: 700, justifyContent: 'center' , textAlign: 'center', display: 'inline-block'}}> 
       <h1>Web Developer</h1>
       <img src={monaPic}alt="Mypic" style={{ display: 'inherit', width: 300, height: 500, borderRadius: 40 }} />
   </div>
