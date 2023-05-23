df5184e57b25dffb05998cb12138c834b10c2485
chunkybooboo <89316044+chunkybooboo@users.noreply.github.com>
2023-05-22 22:02:05 -0700

    made it more responsive

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import monaPic from '../../img/mona.jpg';
 
-
+// style={{ marginInline: 700, justifyContent: 'center' , textAlign: 'center', display: 'inline-block'}}
 export default function Home() {
   return (
-    <div style={{ marginInline: 700, justifyContent: 'center' , textAlign: 'center', display: 'inline-block'}}> 
+    <div
+      className="flex flex-col justify-center items-center text-center"
+    >
       <h1>Web Developer</h1>
-      <img src={monaPic}alt="Mypic" style={{ display: 'inherit', width: 300, height: 500, borderRadius: 40 }} />
-  </div>
+      <img src={monaPic} alt="Mypic" style={{ width: 300, height: 500, borderRadius: 40 }} />
+    </div>
   );
 }
